42b85655fbcf342a9c715f385daaa79a71ed9fc6
gg-script <ddosenka@khismatov.ru>
2025-12-15 05:38:58 +0500

    Add Direction enum and basic game fields

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,12 +1,29 @@
-﻿using System.Windows;
+﻿using System;
+using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Shapes;
 
 namespace SnakeGame
 {
     public partial class MainWindow : Window
     {
+        private List<Rectangle> snakeBody;
+        private int gridSize = 20;
+        private Direction currentDirection;
+        private Random random;
+
         public MainWindow()
         {
             InitializeComponent();
+            random = new Random();
+        }
+
+        private enum Direction
+        {
+            Up,
+            Down,
+            Left,
+            Right
         }
     }
 }
